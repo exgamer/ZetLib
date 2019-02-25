16083c373c5cbaa97d383124ba04f139457e1cc9
citizenzet <exgamer@live.ru>
2019-02-25 17:52:34 +0600

    !

--- restclient/src/main/java/com/citizenzet/restclient/service/SampleService2.java
+++ restclient/src/main/java/com/citizenzet/restclient/service/SampleService2.java
@@ -1,9 +1,14 @@
 package com.citizenzet.restclient.service;
 
 import android.util.Log;
+
 import com.citizenzet.restclient.api.SampleApiService;
+import com.citizenzet.restclient.model.Sample;
 import com.citizenzet.restclient.model.pack.SamplePack;
 import com.google.gson.Gson;
+
+import java.util.List;
+
 import okhttp3.Headers;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -26,8 +31,14 @@ public class SampleService2 extends BaseRestService2<SamplePack>{
         Log.e("!!!!","SUCCESS");
         Log.e("!!!! code",String.valueOf(code));
         Log.e("!!!! headers",headers.toString());
+        SamplePack sp = (SamplePack) body;
+        List<Sample> list = sp.getModels();
+
+
         Gson gson = new Gson();
-        String json = gson.toJson(body);
+        String json = gson.toJson(list);
+
+
         Log.e("!!!!",json);
     }
 
