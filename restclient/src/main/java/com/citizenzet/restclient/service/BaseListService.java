3db99cb72d9d636f9c6bdded39899e64e47d4960
citizenzet <exgamer@live.ru>
2019-03-05 17:36:22 +0600

    ывап

--- restclient/src/main/java/com/citizenzet/restclient/service/BaseListService.java
+++ restclient/src/main/java/com/citizenzet/restclient/service/BaseListService.java
@@ -7,16 +7,6 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public abstract class BaseListService<M> extends BaseRestService<M> {
-    private int currentPage = 0;
-
-    public int getCurrentPage() {
-        return currentPage;
-    }
-
-    public void setCurrentPage(int currentPage) {
-        this.currentPage = currentPage;
-    }
-
     public void request(int page){
         beforeRequest();
         Retrofit retrofit = getBuilder();
