82ec2e30cde6390e61a732b132222c43adb07714
Olzhas <exgamer@live.ru>
2019-03-06 21:44:52 +0600

    xzc

--- restclient/src/main/java/com/citizenzet/restclient/helper/FragmentHelper.java
+++ restclient/src/main/java/com/citizenzet/restclient/helper/FragmentHelper.java
@@ -12,19 +12,34 @@ import com.citizenzet.restclient.fragment.BaseRestFragment;
  */
 public class FragmentHelper {
 
+    public static void setFragment(BaseRestActivity activity, BaseRestFragment fragment, int fragmentElementId){
+        switchFragment(activity, fragment, fragmentElementId, true);
+    }
+
     /**
      * Переключить фрагмент
      * @param activity
      * @param fragment
      */
     public static void switchFragment(BaseRestActivity activity, BaseRestFragment fragment, int fragmentElementId){
+        switchFragment(activity, fragment, fragmentElementId, false);
+    }
+
+    /**
+     * Переключить фрагмент
+     * @param activity
+     * @param fragment
+     */
+    private static void switchFragment(BaseRestActivity activity, BaseRestFragment fragment, int fragmentElementId, boolean isSet){
         // create a FragmentManager
         FragmentManager fm = activity.getSupportFragmentManager();
         // create a FragmentTransaction to begin the transaction and replace the Fragment
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         // replace the FrameLayout with new Fragment
         fragmentTransaction.replace(fragmentElementId, fragment);
-        fragmentTransaction.addToBackStack(null);
+        if (isSet) {
+            fragmentTransaction.addToBackStack(null);
+        }
         fragmentTransaction.commit(); // save the changes
     }
 }
