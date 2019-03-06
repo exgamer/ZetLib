7f21d42c61592ff969bab6a7483758988832240b
citizenzet <exgamer@live.ru>
2019-03-06 14:24:55 +0600

    rt

--- restclient/src/main/java/com/citizenzet/restclient/fragment/BaseRestFragment.java
+++ restclient/src/main/java/com/citizenzet/restclient/fragment/BaseRestFragment.java
@@ -1,8 +1,31 @@
 package com.citizenzet.restclient.fragment;
 
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class BaseRestFragment extends Fragment {
+public abstract class BaseRestFragment<V extends ViewDataBinding,T extends FragmentActivity> extends Fragment {
+    protected T activity;
+    protected V binding;
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding= DataBindingUtil.inflate(inflater, layout(), container, false);
+        afterBinding();
+        return binding.getRoot();
+    }
+
+    public abstract int layout();
+
+    public void afterBinding(){
+
+    }
 }
