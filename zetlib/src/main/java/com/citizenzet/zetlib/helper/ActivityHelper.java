8870995444f44100493bf3f1033481358561f8fa
Olzhas <exgamer@live.ru>
2019-03-17 20:07:35 +0600

    dsfs

--- zetlib/src/main/java/com/citizenzet/zetlib/helper/ActivityHelper.java
+++ zetlib/src/main/java/com/citizenzet/zetlib/helper/ActivityHelper.java
@@ -2,6 +2,8 @@ package com.citizenzet.zetlib.helper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * Вспомогательные методы для работы с activity
@@ -11,5 +13,16 @@ public class ActivityHelper {
     public static void goToActivity(Activity activity, final Class<? extends Activity> activityToGo){
         Intent myIntent = new Intent(activity, activityToGo);
         activity.startActivity(myIntent);
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
