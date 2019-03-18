39e1345a6b7c728f3b0c4392b0a567122db5f89d
citizenzet <exgamer@live.ru>
2019-03-18 13:05:11 +0600

    xfthxasd

---
+++ zetlib/src/main/java/com/citizenzet/zetlib/helper/NotificationHelper.java
@@ -0,0 +1,10 @@
+package com.citizenzet.zetlib.helper;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class NotificationHelper {
+    private static void toast(Activity activity, String message){
+        Toast.makeText(activity.getApplicationContext(),message,Toast.LENGTH_LONG).show();
+    }
+}
