856094b0961ea942efc9c31687c1a1e6ef07e9a6
Olzhas <exgamer@live.ru>
2019-03-27 21:43:35 +0600

    dsfs

--- zetlib/src/main/java/com/citizenzet/zetlib/helper/FragmentHelper.java
+++ zetlib/src/main/java/com/citizenzet/zetlib/helper/FragmentHelper.java
@@ -35,7 +35,7 @@ public class FragmentHelper {
         if (isBack) {
             fragmentTransaction.addToBackStack(backStackKey);
         }else{
-//            fm.popBackStack();
+            fm.popBackStack();
             fragmentTransaction.disallowAddToBackStack();
         }
         fragmentTransaction.commit(); // save the changes
