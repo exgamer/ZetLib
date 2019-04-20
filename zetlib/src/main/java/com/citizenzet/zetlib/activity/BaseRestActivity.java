f2defc851fd00dd9a2cecf03f55fbc17e0d1a50a
Olzhas <exgamer@live.ru>
2019-04-20 15:43:34 +0600

    dsfs

--- zetlib/src/main/java/com/citizenzet/zetlib/activity/BaseRestActivity.java
+++ zetlib/src/main/java/com/citizenzet/zetlib/activity/BaseRestActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.webkit.WebView;
+
 import com.citizenzet.zetlib.application.App;
 
 public class BaseRestActivity extends AppCompatActivity {
@@ -11,6 +13,11 @@ public class BaseRestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (App.isNightModeEnabled()) {
+            /**
+             * Есть багу гугла с темной темой. при инициализации webview сбрасывается тема
+             * пока решается только так
+             */
+            new WebView(getApplicationContext());
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         }else{
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
