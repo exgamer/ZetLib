29ec33e3201bd764fe35e64c7519194067039b3e
citizenzet <exgamer@live.ru>
2019-05-23 11:07:30 +0600

    drt

---
+++ zetlib/src/main/java/com/citizenzet/zetlib/activity/BaseReceiverActivity.java
@@ -0,0 +1,45 @@
+package com.citizenzet.zetlib.activity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+
+public abstract class BaseReceiverActivity extends BaseRestActivity {
+
+    protected String receiverAction = "myAction";
+
+    // Our handler for received Intents. This will be called whenever an Intent
+    // with an action named "custom-event-name" is broadcasted.
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            onReceiveMessage(context, intent);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Register to receive messages.
+        // We are registering an observer (mMessageReceiver) to receive Intents
+        // with actions named "custom-event-name".
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter(receiverAction));
+    }
+    @Override
+    protected void onDestroy() {
+        // Unregister since the activity is about to be closed.
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        super.onDestroy();
+    }
+
+    /**
+     * actions on message received
+     * @param context
+     * @param intent
+     */
+    protected abstract void onReceiveMessage(Context context, Intent intent);
+}
---
+++ zetlib/src/main/java/com/citizenzet/zetlib/service/BaseFirebaseMessagingService.java
@@ -0,0 +1,111 @@
+package com.citizenzet.zetlib.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+
+/**
+ * Created by citizenzet on 11/4/16.
+ */
+
+public abstract class BaseFirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    protected static final String TAG = "FCM Service";
+
+    private NotificationManager mNotificationManager;
+    private static final String ADMIN_CHANNEL_ID = "ADMIN_CHANNEL";
+
+    protected  String serviceMessage = "SERVICE_MESSAGE";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if(remoteMessage==null && remoteMessage.getData()==null &&  remoteMessage.getData().size()==0){
+            return;
+        }
+        notify(remoteMessage.getData());
+        final Intent i = new Intent();
+        i.setAction(serviceMessage);
+        sendBroadcast(i);
+        setupBroadcast(remoteMessage.getData());
+    }
+
+//    protected void setupBroadcast(Map<String, String> data){
+//        Intent intent = new Intent("new_push");
+//        // You can also include some extra data.
+//        intent.putExtra("message", "This is my message!");
+//        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+//    }
+    protected abstract void setupBroadcast(Map<String, String> data);
+
+
+
+//    protected void setupNotificationBuilder(NotificationCompat.Builder mBuilder){
+//        mBuilder.setSmallIcon(R.mipmap.ic_launcher)
+//        .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+//        .setAutoCancel(true)
+//        .setColor(getResources().getColor(R.color.colorAccent))
+//        .setLights(0xFFe2c500, 100, 100)
+//        .setContentTitle(title)
+//        .setGroup("olimp_menu")
+//        .setDefaults(Notification.DEFAULT_ALL)
+//        .setContentText(msg);
+//    }
+    protected abstract void setupNotificationBuilder(NotificationCompat.Builder mBuilder);
+
+//    protected void setupNotification(NotificationCompat.BigTextStyle notification){
+//        notification.bigText(msg).build();
+//    }
+    protected abstract void setupNotification(NotificationCompat.BigTextStyle notification);
+
+//
+//    protected void getResultIntent(){
+//            Intent resultIntent = new Intent(this, MainActivity.class);
+//              resultIntent.putExtra("updateContent", true);
+//    }
+    protected abstract Intent getResultIntent();
+
+    private void notify(Map<String, String> data) {
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //Setting up Notification channels for android O and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setupChannels();
+        }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID);
+        setupNotificationBuilder(mBuilder);
+        Intent resultIntent = getResultIntent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1,
+                resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(pendingIntent);
+        NotificationCompat.BigTextStyle notificationBuild = new NotificationCompat.BigTextStyle(mBuilder);
+        setupNotification(notificationBuild);
+        Notification notification = notificationBuild.build();
+        mNotificationManager.notify(0, notification);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(){
+        CharSequence adminChannelName = "admin";
+        String adminChannelDescription = "admin";
+
+        NotificationChannel adminChannel;
+        adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_LOW);
+        adminChannel.setDescription(adminChannelDescription);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (mNotificationManager != null) {
+            mNotificationManager.createNotificationChannel(adminChannel);
+        }
+    }
+}
