908caacdc6902d7ef6d2ea11cb9698e4a90cb236
citizenzet <exgamer@live.ru>
2019-05-28 11:47:54 +0600

    drt

--- zetlib/src/main/java/com/citizenzet/zetlib/activity/BaseRestActivity.java
+++ zetlib/src/main/java/com/citizenzet/zetlib/activity/BaseRestActivity.java
@@ -1,9 +1,7 @@
 package com.citizenzet.zetlib.activity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.webkit.WebView;
@@ -12,7 +10,7 @@ import com.citizenzet.zetlib.application.App;
 
 public class BaseRestActivity extends AppCompatActivity {
 
-    protected boolean isFullscreen = false;
+//    protected boolean isFullscreen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,38 +37,38 @@ public class BaseRestActivity extends AppCompatActivity {
         }
     }
 
-    public void fullscreenOn() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        isFullscreen = true;
-//        if (Build.VERSION.SDK_INT < 16) {
-//            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        }else{
-//            View decorView = getWindow().getDecorView();
-//            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-//            decorView.setSystemUiVisibility(uiOptions);
+//    public void fullscreenOn() {
+//        // Hide UI first
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.hide();
 //        }
-
-    }
-    
-    public void fullscreenOff() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.show();
-        }
-        isFullscreen = false;
-//        if (Build.VERSION.SDK_INT < 16) {
-//            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        }else{
-//            View decorView = getWindow().getDecorView();
-//            int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
-//            decorView.setSystemUiVisibility(uiOptions);
+//        isFullscreen = true;
+////        if (Build.VERSION.SDK_INT < 16) {
+////            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+////                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+////        }else{
+////            View decorView = getWindow().getDecorView();
+////            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+////            decorView.setSystemUiVisibility(uiOptions);
+////        }
+//
+//    }
+//
+//    public void fullscreenOff() {
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.show();
 //        }
-    }
+//        isFullscreen = false;
+////        if (Build.VERSION.SDK_INT < 16) {
+////            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+////        }else{
+////            View decorView = getWindow().getDecorView();
+////            int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+////            decorView.setSystemUiVisibility(uiOptions);
+////        }
+//    }
 
 
 }
